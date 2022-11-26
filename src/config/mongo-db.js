aae04154e3ace4e7500c52e3bac718f38fea5f34
Alejandro Abraham <alejandro.r.abraham@gmail.com>
2022-11-26 11:43:36 -0300

    fix MONGOOSE_URI in mongo-db.js

--- src/config/mongo-db.js
+++ src/config/mongo-db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import logger from '../utils/logger.js'
 import 'dotenv/config'
 
-const MONGOOSE_URI = process.env.MONGOOSE_URI || 'mongodb://localhost:27017/simcompras'
+import { MONGOOSE_URI } from './constant.js'
 
 const config = {
     mongoDB: {
@@ -17,7 +17,8 @@ const config = {
 export default async () => {
     try {
         await mongoose.connect(config.mongoDB.URL, config.mongoDB.options)
-        logger.info('[MONGODB]: 💾 Connected to MongoDB')
+        const mongoServer = MONGOOSE_URI.split('@')
+        logger.info(`[MONGODB]: 💾 Connected to MongoDB {${mongoServer[1]}}`)
     } catch (error) {
         logger.error(`[MONGODB]: ⚠ MongoDB Error: ${error}`)
     }

6478a0f1fa6e84813fb22d0591c728678bc251cb
Alejandro Abraham <alejandro.r.abraham@gmail.com>
2022-11-26 11:48:52 -0300

    secondsToString. Seconds round to 2 decimal places

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -9,7 +9,7 @@ export const secondsToString = function (seconds) {
         let minute = Math.floor((seconds / 60) % 60)
         minute = (minute < 10) ? '0' + minute : minute
         let second = seconds % 60
-        second = (second.toFixed() < 10) ? '0' + second : second
+        second = (second.toFixed(2) < 10) ? '0' + second : second
         return days + ' dias ' + hour + ':' + minute + ':' + second
     } catch (error) {
         return { error: `${error}` }
